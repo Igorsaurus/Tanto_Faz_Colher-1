5f6d7ad8e9410aafccb2f47024297e6298cebfdc
Igorsaurus <i.ponci12@gmail.com>
2020-04-17 11:02:03 -0300

    laser laser laser

---
+++ Tanto faz Colher com menu/Assets/Scripts/CodigoLaser.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CodigoLaser : MonoBehaviour
+{
+    public float distance;
+    public GerenciadorDeFase GerenciadorDeFase;
+    public LayerMask Colidivel;
+    public LineRenderer lineOfSight;
+    public Gradient corEscura;
+    public Gradient corClara;
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit2D hitRaycast = Physics2D.Raycast(transform.position, transform.right, distance,Colidivel);
+        if(hitRaycast.collider != null){
+            Debug.DrawLine(transform.position, hitRaycast.point, Color.red);
+            lineOfSight.SetPosition(1, hitRaycast.point);
+            lineOfSight.colorGradient = corEscura;
+        }
+        else
+        {
+            Debug.DrawLine(transform.position, transform.position + transform.right * distance, Color.yellow);
+            lineOfSight.SetPosition(1, transform.position + transform.right * distance);
+            lineOfSight.colorGradient = corClara;
+        }
+        if (hitRaycast.collider.CompareTag("Player"))
+        {
+            GerenciadorDeFase.RespawnPlayer();
+        }
+        lineOfSight.SetPosition(0, transform.position);
+    }
+    private void Start()
+    {
+        Physics2D.queriesStartInColliders = false;
+        GerenciadorDeFase = FindObjectOfType<GerenciadorDeFase>();
+    }
+}
---
+++ Tanto faz Colher com menu/Assets/Scripts/ControleLaser.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControleLaser : MonoBehaviour
+{
+    private LineRenderer lineRenderer;
+    public Transform hitLaser;
+    public GerenciadorDeFase GerenciadorDeFase;
+    public float distance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.enabled = true;
+        lineRenderer.useWorldSpace = true;
+        GerenciadorDeFase = FindObjectOfType<GerenciadorDeFase>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.up, distance);
+        Debug.DrawLine(transform.position, hit.point);
+        hitLaser.position = hit.point;
+        lineRenderer.SetPosition(0, transform.position);
+        lineRenderer.SetPosition(1, hitLaser.position);
+       
+        if (hit.collider.name == "colher")
+        {
+            GerenciadorDeFase.RespawnPlayer();
+        }
+
+    }
+}
